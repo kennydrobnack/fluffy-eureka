d485100fc853fe2267e31befe4956943e3b0f81c
Michael <NA>
2017-01-21 12:54:07 -0500

    Playing with a scene setup for comparing waves

---
+++ Fluffy Eureka Neo Shark/Assets/Scripts/Impulse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulse : MonoBehaviour {
+
+	public Vector2 impulse;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = impulse;
+	}
+
+}
