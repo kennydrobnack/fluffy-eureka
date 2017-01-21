252f107624621b8eb49ed9ce2130c5303579a617
Michael <NA>
2017-01-21 08:43:31 -0500

    Added functionality to pause script

--- Fluffy Eureka Neo Shark/Assets/PauseButton.cs
+++ Fluffy Eureka Neo Shark/Assets/PauseButton.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PauseButton : MonoBehaviour {
 
-	public void Pause(){}
-	public void UnPause(){}
+	public void Pause(){
+		SetTimeScale (0f); 
+	}
+	public void UnPause(){
+		SetTimeScale (1f);
+	}
+
+	public void SetTimeScale(float scale) {
+		Time.timeScale = scale;
+	}
 }
