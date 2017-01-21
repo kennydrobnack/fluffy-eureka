c4726793f2abca530212582201a1970071bae873
Kenny Drobnack <kennydrobnack@gmail.com>
2017-01-21 17:35:33 -0500

    Fixed fish going in the wrong direction.

--- Fluffy Eureka Neo Shark/Assets/Scripts/FishSpawner.cs
+++ Fluffy Eureka Neo Shark/Assets/Scripts/FishSpawner.cs
@@ -31,7 +31,7 @@ public class FishSpawner : MonoBehaviour {
         aFish.transform.position = new Vector2(Random.Range(min.x, max.x), min.y);
         float x_impulse = Random.Range(min_x_impulse, max_x_impulse);
         float y_impulse = Random.Range(min_y_impulse, max_y_impulse);
-        if (aFish.transform.position.x <= 0.5)
+        if (aFish.transform.position.x >= 0.5)
         {
             x_impulse = -x_impulse;
         }
