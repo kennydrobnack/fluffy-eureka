b0c80be589b25bd572ecb90c09691cd558c33f1f
Michael <NA>
2017-01-21 08:33:56 -0500

    Added basic pause screen/functionality

---
+++ Fluffy Eureka Neo Shark/Assets/PauseButton.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour {
+
+	public void Pause(){}
+	public void UnPause(){}
+}
