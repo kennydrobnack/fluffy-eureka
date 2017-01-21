d78ff05db91524f141a5f0bedcc5cff258724069
Michael <NA>
2017-01-21 12:44:36 -0500

    Added character controller

---
+++ Fluffy Eureka Neo Shark/Assets/JumpCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCheck : MonoBehaviour {
+
+	Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();	
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		anim.SetBool ("isJumping", false);
+	}
+
+	void OnCollisionExit2D(Collision2D other) {
+		anim.SetBool ("isJumping", true);
+	}
+}
