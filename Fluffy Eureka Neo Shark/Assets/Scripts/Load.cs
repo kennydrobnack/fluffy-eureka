71fae5b92ac0e9143485f4fb844cd4d7f35f6f46
Michael <NA>
2017-01-21 01:30:08 -0500

    Added basic Main Menu -> Game logic

---
+++ Fluffy Eureka Neo Shark/Assets/Scripts/Load.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour {
+
+	public void LoadLevel(int index) {
+		SceneManager.LoadScene (index);
+	}
+}
---
+++ Fluffy Eureka Neo Shark/Assets/Scripts/Quit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+
+	public void QuitGame(){
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#elif
+		Application.Quit();
+#endif
+	}
+}
