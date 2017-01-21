c2ab5fe290250b4ff57277c4d9250fbef143e4cf
Michael <NA>
2017-01-21 12:11:04 -0500

    Added basic jumping

---
+++ Fluffy Eureka Neo Shark/Assets/Scripts/TestJump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class TestJump : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			anim.SetBool ("isJumping", true);
+		} else {
+			anim.SetBool ("isJumping", false);
+		}
+	}
+}
